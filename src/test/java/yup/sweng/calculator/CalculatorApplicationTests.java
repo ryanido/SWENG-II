ed0c3aaee4294d0d677f5ff0f04cf6a086f58751
ryanido <ryanidowu@gmail.com>
2022-10-25 22:57:46 +0100

    changed tests to see what happens if fails

--- src/test/java/yup/sweng/calculator/CalculatorApplicationTests.java
+++ src/test/java/yup/sweng/calculator/CalculatorApplicationTests.java
@@ -14,7 +14,7 @@ class CalculatorApplicationTests {
 
     @Test
     void testWelcome() {
-        assert welcome().equals("Welcome to calcuator");
+        assert welcome().equals("Welcome to not calcuator");
     }
 
 }
